db6f0db1693b3686203b0e3bbc1e5873b624545f
LeonardTarigan <leontrg32@gmail.com>
2026-07-22 20:01:42 +0700

    chore(web): setup web

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,29 @@
+import MainProvider from '#/shared/components/providers/MainProvider'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import '@mantine/core/styles.css'
+import '../styles.css'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <MainProvider>
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'TanStack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </MainProvider>
+  )
+}
---
+++ apps/web/src/shared/components/providers/MainProvider.tsx
@@ -0,0 +1,16 @@
+import mainTheme from '#/shared/theme/main.theme'
+import { MantineProvider } from '@mantine/core'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { ReactNode } from 'react'
+
+const queryClient = new QueryClient()
+
+const MainProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MantineProvider theme={mainTheme}>{children}</MantineProvider>
+    </QueryClientProvider>
+  )
+}
+
+export default MainProvider
