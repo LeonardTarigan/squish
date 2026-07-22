d61bfe596e6181eb155ecfc82a1cfd8de9a82589
LeonardTarigan <leontrg32@gmail.com>
2026-07-22 20:06:15 +0700

    feat(web): setup theme color

---
+++ apps/web/src/shared/theme/colors/primary.color.ts
@@ -0,0 +1,16 @@
+import type { MantineColorsTuple } from "@mantine/core"
+
+const primaryColor: MantineColorsTuple = [
+    '#fff5e1',
+    '#ffeacd',
+    '#fbd49e',
+    '#f7bd6b',
+    '#f4a73b',
+    '#f39d24',
+    '#f29613',
+    '#d88204',
+    '#c07300',
+    '#a76200'
+]
+
+export default primaryColor
--- apps/web/src/shared/theme/main.theme.ts
+++ apps/web/src/shared/theme/main.theme.ts
@@ -1,5 +1,11 @@
 import { createTheme } from "@mantine/core";
+import primaryColor from "./colors/primary.color";
 
-const mainTheme = createTheme({})
+const mainTheme = createTheme({
+    colors: {
+        primary: primaryColor
+    },
+    primaryColor: 'primary'
+})
 
 export default mainTheme
