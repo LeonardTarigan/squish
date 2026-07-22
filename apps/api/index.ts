17f91f5d8f7d0346b3b8c26d3808bce5658ffad2
LeonardTarigan <leontrg32@gmail.com>
2026-07-22 18:21:49 +0700

    feat(api): setup cors

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,10 +2,23 @@ import { logger } from '@squish/logger'
 import { Hono } from 'hono'
 import { pinoLogger } from 'hono-pino'
 import { apiRouter } from './routes'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
+const rawOrigins = process.env.ALLOWED_ORIGINS;
+if (!rawOrigins) {
+    throw new Error('Environment variable ALLOWED_ORIGINS is missing');
+}
+
+const allowedOrigins = rawOrigins.split(',')
+
 app.use('*', pinoLogger({ pino: logger }))
+app.use('*', cors({
+    origin: (origin) => allowedOrigins.includes(origin) ? origin : null,
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization']
+}))
 
 app.get('/health', (c) => {
     return c.json({ status: 'ok', service: 'squish-api' })
