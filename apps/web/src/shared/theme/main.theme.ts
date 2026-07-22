76dffb997db962ca7b813f2379ffb0e17f4ef5a9
LeonardTarigan <leontrg32@gmail.com>
2026-07-22 20:13:33 +0700

    feat(web): setup font

--- apps/web/src/shared/theme/main.theme.ts
+++ apps/web/src/shared/theme/main.theme.ts
@@ -2,6 +2,7 @@ import { createTheme } from "@mantine/core";
 import primaryColor from "./colors/primary.color";
 
 const mainTheme = createTheme({
+    fontFamily: 'DM Sans, sans-serif',
     colors: {
         primary: primaryColor
     },
