26f96d81e97b6b78e81dc6cab5fffd621137db7e
LeonardTarigan <leontrg32@gmail.com>
2026-07-27 07:12:53 +0700

    chore: adjust vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
@@ -6,14 +6,21 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const config = defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
-    viteReact(),
-  ],
+const config = defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    resolve: { tsconfigPaths: true },
+    server: {
+      port: parseInt(env.VITE_PORT || '3001', 10),
+      strictPort: true
+    },
+    plugins: [
+      devtools(),
+      tailwindcss(),
+      tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+      viteReact(),
+    ],
+  }
 })
 
 export default config
